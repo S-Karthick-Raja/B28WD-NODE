f4e54a3a9b9388320895a8a2b039c484c35327c1
S-Karthick-Raja <kraja2796@gmail.com>
2021-12-02 01:25:58 +0530

    updated

--- helper.js
+++ helper.js
@@ -1,47 +1,37 @@
 import { ObjectId } from "mongodb";
 import { client } from "./index.js";
 
-
- async function getMovies() {
-    return await client
-        .db("test")
-        .collection("movies")
-        .find({})
-        .toArray();
+async function getMovies() {
+  return await client.db("test").collection("movies").find({}).toArray();
 }
- async function createMovies(data) {
-    return await client.db("test").collection("movies").insertMany(data);
+async function createMovies(data) {
+  return await client.db("test").collection("movies").insertMany(data);
 }
 
-async function createMoviesone(data) {
-    return await client.db("test").collection("movies").insertOne(data);
+async function deleteMovieById(id) {
+  return await client
+    .db("test")
+    .collection("movies")
+    .deleteOne({ _id: ObjectId(id) });
 }
-
- async function deleteMovieById(id) {
-    return await client
-        .db("test")
-        .collection("movies")
-        .deleteOne({ _id:  ObjectId(id) });
+async function UpdateMovieById(id, data) {
+  return await client
+    .db("test")
+    .collection("movies")
+    .updateOne({ _id: ObjectId(id) }, { $set: data });
 }
- async function UpdateMovieById(id, data) {
-    return await client
-        .db("test")
-        .collection("movies")
-        .updateOne({ _id:  ObjectId(id) }, { $set: data });
-}
- async function getMovieById(id) {
-     console.log("***", id)
-    return await client
-        .db("test")
-        .collection("movies")
-        .findOne({ _id: ObjectId(id) });
+async function getMovieById(id) {
+  console.log("***", id);
+  return await client
+    .db("test")
+    .collection("movies")
+    .findOne({ _id: ObjectId(id) });
 }
 
 export {
-    getMovies,
-    createMovies,
-    deleteMovieById,
-    UpdateMovieById,
-    getMovieById,
-    createMoviesone,
+  getMovies,
+  createMovies,
+  deleteMovieById,
+  UpdateMovieById,
+  getMovieById,
 };
